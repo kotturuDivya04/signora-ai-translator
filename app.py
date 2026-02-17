6040e753f8bab2626f8a0a494af8a10ba1d38f96
Divya Kotturu <divyak121654@gmail.com>
2026-02-17 09:49:17 +0530

    updated route app

--- app.py
+++ app.py
@@ -12,6 +12,10 @@ CORS(app)
 # ==============================
 
 @app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/home")
 def home():
     return render_template("home.html")
 
@@ -91,3 +95,4 @@ def predict():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
+
